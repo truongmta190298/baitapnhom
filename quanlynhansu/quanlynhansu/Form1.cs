8cb1dac27da9599364f94b238a5c7200228ecb84
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-09-13 00:56:36 +0100

    add panel and rename form1 to f1

--- quanlynhansu/quanlynhansu/Form1.cs
+++ quanlynhansu/quanlynhansu/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace quanlynhansu
 {
-    public partial class Form1 : Form
+    public partial class f1 : Form
     {
-        public Form1()
+        public f1()
         {
             InitializeComponent();
         }
